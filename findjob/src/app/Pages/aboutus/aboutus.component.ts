8da384e6e5d397aa0527501f81a1b66c18db96b5
Andrew-Yan91 <andrewyan91@hotmail.com>
2018-12-13 14:10:54 +1300

    s

---
+++ findjob/src/app/Pages/aboutus/aboutus.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-aboutus',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutusComponent implements OnInit {
+  ngOnInit() {}
+  constructor(
+    ) {}
+ 
+  }
+
+
+
---
+++ findjob/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'findjob';
+}
