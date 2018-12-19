9ef17e29d7f5557f7e9b6bda59c655d09e30b056
Angela <xoneonxero@gmail.com>
2018-12-19 16:42:16 +1300

    basic faq

--- src/app/Pages/faq/faq.component.ts
+++ src/app/Pages/faq/faq.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
+  questions: any[] = [
+    {
+      question: 'Who is the best character in PMMM?',
+      answer: 'Kyubey.'
+    },
+    {
+      question: 'Why did Cleopatra fall to despair?',
+      answer: 'Kyubey.'
+    },
+    {
+      question: 'What is a famous restaurant in Japan?',
+      answer: 'Kyubey.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
