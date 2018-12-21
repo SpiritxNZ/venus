03ece5eab34b20d7d19d93c28b0810693e9107d7
Andrew-Yan91 <andrewyan91@hotmail.com>
2018-12-21 13:02:25 +1300

    adjusted layout

--- src/app/programs/programsmain/programsmain.component.ts
+++ src/app/programs/programsmain/programsmain.component.ts
@@ -11,53 +11,5 @@ export class ProgramsmainComponent implements OnInit {
   ngOnInit() {
 
   }
-  showbtn1()
-  {
-    document.getElementById("learnmorebtn1").style.opacity="1";
-  }
-  hidebtn1()
-  {
-    document.getElementById("learnmorebtn1").style.opacity="0";
-  }
-  showbtn2()
-  {
-    document.getElementById("learnmorebtn2").style.opacity="1";
-  }
-  hidebtn2()
-  {
-    document.getElementById("learnmorebtn2").style.opacity="0";
-  }
-  showbtn3()
-  {
-    document.getElementById("learnmorebtn3").style.opacity="1";
-  }
-  hidebtn3()
-  {
-    document.getElementById("learnmorebtn3").style.opacity="0";
-  }
-  showbtn4()
-  {
-    document.getElementById("learnmorebtn4").style.opacity="1";
-  }
-  hidebtn4()
-  {
-    document.getElementById("learnmorebtn4").style.opacity="0";
-  }
-  showbtn5()
-  {
-    document.getElementById("learnmorebtn5").style.opacity="1";
-  }
-  hidebtn5()
-  {
-    document.getElementById("learnmorebtn5").style.opacity="0";
-  }
-  showbtn6()
-  {
-    document.getElementById("learnmorebtn6").style.opacity="1";
-  }
-  hidebtn6()
-  {
-    document.getElementById("learnmorebtn6").style.opacity="0";
-  }
-    
+  
 }
