56b8ba76408566e2368fd82ce2943bc0212dc47d
Mike <info@learnspace.co.nz>
2019-01-15 09:27:12 +1300

    Update

--- src/app/successcases/successcases.component.ts
+++ src/app/successcases/successcases.component.ts
@@ -10,29 +10,37 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class SuccesscasesComponent implements OnInit {
   video1:any;
   video2:any;
+  video3:any;
 
   videoPerson1 = {
     name: 'Neo',
     uni: 'ICL Business School',
     job: 'Junior Developer',
     jobLoc: 'Hatch Web & Design',
-    description: 'Neo came to us in March 2017 while he was still attending \
-    ICL business school in Auckland. In less than 6 months, Neo came from only \
-    basic knowledge in HTML and CSS to being able to proficiently use the \
-    Angular framework with JavaScript and TypeScript, and able to use the \
-    Laravel framework (PHP) as a RESTful API.'
+    description: `Neo came to us in March 2017 while he was still attending 
+    ICL business school in Auckland. In less than 6 months, Neo came from only 
+    basic knowledge in HTML and CSS to being able to proficiently use the 
+    Angular framework with JavaScript and TypeScript, and able to use the 
+    Laravel framework (PHP) as a RESTful API.`
   }
   videoPerson2 = {
     name: 'Bill',
     uni: 'AUT',
     job: 'Junior Developer',
     jobLoc: 'Lancom Technology',
-    description: 'Bill came to us in June 2017 after he graduated from AUT. \
-    While at Gradspace, Bill learnt and entered the depth of the Angular \
-    framework, developed essential leadership skills while leading a team \
-    of graduates to complete a major commercial project.'
+    description: `Bill came to us in June 2017 after he graduated from AUT. 
+    While at Gradspace, Bill learnt and entered the depth of the Angular 
+    framework, developed essential leadership skills while leading a team 
+    of graduates to complete a major commercial project.`
   }
-
+  videoPerson3 = {
+    name: 'Pirlo',
+    uni: 'AUT',
+    job: 'Intermediate Developer',
+    jobLoc: 'Parkable',
+    description: `Pirlo came to us in July 2017. While at Gradspace, 
+    Pirlo learnt the Angular framework, NodeJS and used them to create a commercial web based chatting application.`
+  }
   successfulpeople: any[] = [
     {
       "image":"../../assets/tutor_images/successcasesirene.jpg",
@@ -54,6 +62,7 @@ export class SuccesscasesComponent implements OnInit {
   constructor(private sanitizer: DomSanitizer) { 
     this.video1 = sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/Nbpl_8ijhUo");
     this.video2 = sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/qw2b4HEgoeA");
+    this.video3 = sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/EYd_USvuDdI");
   }
   ngOnInit() {
   }
