6f4097f3e6a4f7157adfacc5a4c771b92c7b77e6
unknown <yuyuanding0@gmail.com>
2019-04-10 14:42:36 +1200

    Update 101

--- src/app/success-cases/success-cases.component.ts
+++ src/app/success-cases/success-cases.component.ts
@@ -49,21 +49,44 @@ videoPersons: any[] = [
   }
 ];
 
-  successfulpeople: any[] = [
-    {
-      "image":"../../assets/tutor_images/successcasesmark.jpg",
-      "name": "Mark",
-      "university":"University of Auckland",
-      "offerlocation":"Sands Software",
-      "offerposition":"Junior Developer",
-      "courses":"Software Developer Programme",
-      "details":`During my time at Gradspace, I learned not only the tech skills such as Angular, Javascript, 
-      CSS and Node.js but also soft skills like communication skills, 
-      time management skills, teamwork and problem-solving skills. Those soft skills are which you can’t get from the university. 
-      It helps me a lot to improve both my tech skills and soft skills til finally, I get a job from Macau as an IT developer. 
-      Highly recommend someone who graduated from university but doesn’t have many working experiences. `
-    }
-  ];
+
+imgPersons: any[]=[
+  {
+    image:'../../assets/tutor_images/successcasesirene.jpg',
+    name:'Irene',
+    uni:'University of Auckland',
+    job:' Junior Developer',
+    jobLoc:'Magiq Software',
+    description:"Gradspace helped me to improve my technical skills and lots of smart tips on how to make myself more employable. You also get lots of chances to work on real live projects with experienced developers in the programme. It's no surprise that you'll gain heaps of practical experience from the programme that you don’t get from the university. Thanks Gradspace for providing this fantastic program for IT graduates."
+  },
+  { 
+  image:'../../assets/tutor_images/successcasesmark.jpg',
+  name:'Mark',
+  uni:'University of Auckland',
+  job:' Junior Developer',
+  jobLoc:'Sands Software',
+  description:'During my time at Gradspace, I learned not only the tech skills such as Angular, Javascript, CSS and Node.js but also soft skills like communication skills, time management skills, teamwork and problem-solving skills. Those soft skills are which you can’t get from the university. It helps me a lot to improve both my tech skills and soft skills til finally, I get a job from Macau as an IT developer. Highly recommend someone who graduated from university but doesn’t have many working experiences.'
+  
+  },
+  {
+  image:'../../assets/tutor_images/successcasessnow.jpg',
+  name:'Snow',
+  uni:'University of Auckland',
+  job:'Software tester',
+  jobLoc:'Planit',
+  description:"It was lucky for me to be an internship in Gradspace before. Since I not only learnt technical skills such as Angular framework, Javascript and CSS programming languages but also gained hands-on experiences in different projects, which is truly helpful and useful when applying for jobs and even using in work. More importantly, it’s not like knowledge which can learn from the university. Huge thanks to Gradspace and recommend graduates who are looking for valuable experiences but no idea where to start."
+  },
+
+  {
+  image:'../../assets/tutor_images/successcasesdean.jpg',
+  name:'Dean',
+  uni:'Unitec Institute of Technology',
+  job:'Software tester',
+  jobLoc:'Gentrack',
+  description:' I came to Gradspace when I was in my second year at Unitec studying Computing, now still in my third year and haven’t graduated from Unitec yet, I have already found and signed onto a software testing job. At Gradspace not only you learn the most popular coding tools on the market, but more importantly, you get to try out on a real project with a real team which is something you won’t get to do in university. Recommended to anyone who wants to get good employment in IT.',
+  }
+]
+
   
   constructor(
     private translate: TranslateService,
